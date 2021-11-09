ba43aba3406ac4ba8829fd7abb6b7135614d8374
petifox <73014276+petifox@users.noreply.github.com>
2021-11-09 11:39:38 +0100

    Oxygen

---
+++ AzimuthV2_urp/Assets/Scripts/OxygenController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OxygenController : MonoBehaviour
+{
+    public float speed;
+    public Slider slider;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        slider.value -= Mathf.Abs(Mathf.Sin(Time.time) * speed * Time.deltaTime);
+    }
+}
